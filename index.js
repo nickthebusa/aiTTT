6eb34d6a429d11838134f43c95fa2397f4ec06d4
Nick Busa <nbusa1231@gmail.com>
2025-02-19 18:37:43 -0500

    second-commit

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   gameSquares.forEach((square, index) => {
     square.addEventListener("click", () => {
-      
       // determine row
       const row = Math.floor(index / 3);
       // determine column
